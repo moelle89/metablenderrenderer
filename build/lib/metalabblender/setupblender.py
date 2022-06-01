592fdd15607dd26a65bde9e76d234d0e02f28702
Anton Dias <antondias4@gmail.com>
2022-06-01 17:11:09 +0530

    more blender versions added

--- build/lib/metalabblender/setupblender.py
+++ build/lib/metalabblender/setupblender.py
@@ -1,7 +1,23 @@
 import os
 import subprocess
 
-blender_url_dict = {'2.79b'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2",
+blender_url_dict = {'2.70'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.70/blender-2.70-linux-glibc211-x86_64.tar.bz2",
+                    '2.70a'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.70/blender-2.70a-linux-glibc211-x86_64.tar.bz2",
+                    '2.71'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.71/blender-2.71-linux-glibc211-x86_64.tar.bz2",
+                    '2.72'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.72/blender-2.72-linux-glibc211-x86_64.tar.bz2",
+                    '2.72a'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.72/blender-2.72a-linux-glibc211-x86_64.tar.bz2",
+                    '2.72b'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.72/blender-2.72b-linux-glibc211-x86_64.tar.bz2",  
+                    '2.73'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.73/blender-2.73-linux-glibc211-x86_64.tar.bz2",
+                    '2.74'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.74/blender-2.74-linux-glibc211-x86_64.tar.bz2",
+                    '2.75'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.75/blender-2.75-linux-glibc211-x86_64.tar.bz2",
+                    '2.75a'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.75/blender-2.75a-linux-glibc211-x86_64.tar.bz2",
+                    '2.76'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.76/blender-2.76-linux-glibc211-x86_64.tar.bz2",
+                    '2.76b'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.76/blender-2.76b-linux-glibc211-x86_64.tar.bz2",
+                    '2.77'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.77/blender-2.77-linux-glibc211-x86_64.tar.bz2",
+                    '2.78'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.78/blender-2.78-linux-glibc211-x86_64.tar.bz2",
+                    '2.78c'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.78/blender-2.78c-linux-glibc219-x86_64.tar.bz2",
+                    '2.79'    : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.79/blender-2.79-linux-glibc219-x86_64.tar.bz2",
+                    '2.79b'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2",
                     '2.80rc3' : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.80/blender-2.80rc3-linux-glibc217-x86_64.tar.bz2",
                     '2.81a'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.81/blender-2.81a-linux-glibc217-x86_64.tar.bz2",
                     '2.82a'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.82/blender-2.82a-linux64.tar.xz",
@@ -11,6 +27,8 @@ blender_url_dict = {'2.79b'   : "https://ftp.nluug.nl/pub/graphics/blender/relea
                     '2.92.0'  : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.92/blender-2.92.0-linux64.tar.xz",
                     '2.93.8'  : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.93/blender-2.93.8-linux-x64.tar.xz",
                     '3.0.1'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.0/blender-3.0.1-linux-x64.tar.xz",
+                    '3.1.0'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.1/blender-3.1.0-linux-x64.tar.xz",
+                    '3.1.1'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.1/blender-3.1.1-linux-x64.tar.xz",
                     '3.1.2'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.1/blender-3.1.2-linux-x64.tar.xz"}
 
 def setup(blenderVersion):
