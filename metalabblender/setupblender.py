d66a2b806d885205c83a1410457be221acc0a50e
moelle89 <manuel.moellmann@gmail.com>
2022-08-05 03:24:17 +0200

    Update setupblender.py

--- metalabblender/setupblender.py
+++ metalabblender/setupblender.py
@@ -30,7 +30,7 @@ blender_url_dict = {'2.70'    : "https://ftp.nluug.nl/pub/graphics/blender/relea
                     '3.1.0'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.1/blender-3.1.0-linux-x64.tar.xz",
                     '3.1.1'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.1/blender-3.1.1-linux-x64.tar.xz",
                     '3.1.2'   : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.1/blender-3.1.2-linux-x64.tar.xz",
-                    '3.2.0'     : "https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.2/blender-3.2.0-linux-x64.tar.xz"}
+                    '3.2.0'     : "https://www.dropbox.com/s/r9xrustrvbk67jn/E-Cycles_RTX_3.2_release_20220611_lin.tar.bz2?dl=1"}
 
 def setup(blenderVersionOrUrl , isBlenderUrl):
     blender_url = None
